7df15380814bda486cdb1bcede3334ba69487789
jkylecarpentry@gmail.com <jkylecarpentry@gmail.com>
2019-08-26 16:18:19 -0400

    Add project files.

---
+++ WebAPiDemoProject/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPiDemoProject.Models {
+    public class Product 
+    {
+        public int ProductId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ WebAPiDemoProject/Models/WebServiceDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPiDemoProject.Models {
+    public class WebServiceDb : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
+        public WebServiceDb(DbContextOptions options) : base(options) { }
+
+    }
+    
+}
